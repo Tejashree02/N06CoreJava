2d49fd49f271bf5398836a8f5ac9483e770a82fd
Tejashree <Tejashree@LAPTOP-ODTB4Q4S>
2024-01-14 20:45:49 +0530

    second commit

--- ShoppingSpringBootProject/src/main/java/com/example/demo/ShoppingSpringBootPojectApplication.java
+++ ShoppingSpringBootProject/src/main/java/com/example/demo/ShoppingSpringBootPojectApplication.java
@@ -1,13 +1,14 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SampleSpringBootProjectApplication {
+public class ShoppingSpringBootPojectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SampleSpringBootProjectApplication.class, args);
+		SpringApplication.run(ShoppingSpringBootPojectApplication.class, args);
 	}
 
 }
--- ShoppingSpringBootProject/src/test/java/com/example/demo/ShoppingSpringBootProjectApplicationTests.java
+++ ShoppingSpringBootProject/src/test/java/com/example/demo/ShoppingSpringBootProjectApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SampleSpringBootProjectApplicationTests {
+class ShoppingSpringBootProjectApplicationTests {
 
 	@Test
 	void contextLoads() {
